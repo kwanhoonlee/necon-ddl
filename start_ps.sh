fa04e85449d0df49a7adf1d49143e8eaea3cc3ce
may21 <may21.lee@gmail.com>
2020-08-14 15:55:30 +0900

    modified scripts

--- start_ps.sh
+++ start_ps.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 NUM_CPUS=2
-IPADDRESS=172.24.0.
+PS_IPADDR=172.24.0.
+IPADDRESS=172.23.0.
 BRIDGE="br-n-ddl"
 PORT=5012
 BENCHMARK_DIR_PATH="/home/test/Documents/github.com/benchmarks/:/mnt/benchmarks/"
+THREADS=2
+
+for i in $(seq 3 $(($NUM_NODES+2)));
+do
+	ADDRESS+="${IPADDRESS}${i}:${PORT},"
+done
 
 docker run --shm-size="8g" --name p0 --network $BRIDGE \
 -v $BENCHMARK_DIR_PATH -p ${PORT}/tcp -d tensorflow/tensorflow:1.12.0-rc2 \
 docker update --cpus=$NUM_CPUS w$i
 
+
+docker exec -d w$i python ../mnt/benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py \
+--batch_size=64 --model=${MODEL} --variable_update=parameter_server --data_format=NHWC \
+--job_name=worker --ps_hosts=${PS_IPADDR}2:${PORT} \
+--worker_hosts=${ADDRESS} --num_intra_threads=${THREADS} --num_inter_threads=${THREADS} \
+--data_name=imagenet --cross_replica_sync=true --task_index=${i}
 
 
--- start_worker.sh
+++ start_worker.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 NUM_NODES=$1
 NUM_CPUS=4
+
+PS_IPADDR=172.24.0.
 IPADDRESS=172.23.0.
+ADDRESS=""
 BRIDGE="br-n-ddl"
 PORT=5012
 BENCHMARK_DIR_PATH="/home/oslab2/Documents/github.com/benchmarks/:/mnt/benchmarks/"
+
+THREADS=4
+
+for i in $(seq 3 $(($NUM_NODES+2)));
+do
+	ADDRESS+="${IPADDRESS}${i}:${PORT},"
+done
+
+ADDRESS=${ADDRESS::-1}
+echo $ADDRESS
 
 for (( i=0; i<$NUM_NODES; i=i+1 ))
 do
@@ -12,6 +25,18 @@ do
         -v $BENCHMARK_DIR_PATH -p ${PORT}/tcp -d tensorflow/tensorflow:1.12.0-rc2 \
 
 	docker update --cpus=$NUM_CPUS w$i
+done
+
+for (( i=0; i<$NUM_NODES; i=i+1 ))
+do
+	echo $i
+	sleep 1s
+	docker exec -d w$i python ../mnt/benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py \
+	--batch_size=64 --model=${MODEL} --variable_update=parameter_server --data_format=NHWC \
+	--job_name=worker --ps_hosts=${PS_IPADDR}2:${PORT} \
+	--worker_hosts=${ADDRESS} --num_intra_threads=${THREADS} --num_inter_threads=${THREADS} \
+	--data_name=imagenet --cross_replica_sync=true --task_index=${i}
+
 done
 
 
